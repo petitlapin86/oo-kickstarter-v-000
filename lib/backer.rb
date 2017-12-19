e3cd8d6668dfb083863454a8c2ff5d8bcfd26654
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 20:14:20 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,17 +1,14 @@
 class Backer
-
   attr_reader :name, :backed_projects
 
-def initialize(name) #takes a name on initialization
-  @name = name
-  backed_projects = [] # empty array
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
+
 end
-
-
-
-def back_project(project)
-  backed_projects << project # accepts a Project as an argument and stores it in a backed_projects array
-      project.backers << self  #also adds the backer to the project's backers array
-end
-
-end # ends class
--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 
 class Project
-  attr_reader :title, :backers 
+  attr_reader :title, :backers
 
-  def initialize(title) #initialize with argument of title 
+  def initialize(title) #initialize with argument of title
     @title = title
-    @backers = [] # create empty array 
+    @backers = [] # create empty array
   end
 
-  def add_backer(backer) # take in a new backer as an argument 
-    backers << backer # add backer to backers array 
+  def add_backer(backer) # take in a new backer as an argument
+    backers << backer # add backer to backers array
     backer.backed_projects << self
   end
 
