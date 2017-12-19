5c6cad218a77775489d6337381533bab764b660d
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 20:05:25 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,15 @@
 class Project
 
-  attr_reader :title
+  attr_reader :title, :backers
 
 def initialize(title)
 @title = tile
+@backers = []
 end
 
+
+def add_backer(project)
+  backers << project # accepts a Project as an argument and stores it in a backed_projects array
+end
 
 end #end class
