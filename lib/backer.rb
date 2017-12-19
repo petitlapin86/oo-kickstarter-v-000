ebf2155c95c92377ae74403454d3762c9fdc89d1
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 20:00:23 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer
 
   attr_accessor :name
-  
+
 def initialize #takes a name on initialization
   @name = name
 end
 
-backed_projects = []
+backed_projects = [] # empty array 
 
 def back_project(Project)
-  backed_projects << project 
+  backed_projects << project # accepts a Project as an argument and stores it in a backed_projects array
 
 end # ends class
