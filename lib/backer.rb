55af00bfa8a9aecd0e81897001f4099bae9d20c4
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 20:10:56 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,6 +11,7 @@ end
 
 def back_project(project)
   backed_projects << project # accepts a Project as an argument and stores it in a backed_projects array
+      project.backers << self  #also adds the backer to the project's backers array
 end
 
 end # ends class
